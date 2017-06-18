9412dc0b8be10c8f00f7b456e76dcd772f7362a1
Cindy Soares <cindy.as@gmail.com>
2017-06-17 22:09:12 -0300

    popups for new donors and markers for donors

--- Code/src/app/esri-map/esri-map.component.ts
+++ Code/src/app/esri-map/esri-map.component.ts
@@ -64,7 +64,7 @@ export class EsriMapComponent implements OnInit {
 
         if(this.location) {
         	mapViewProperties['center']=[this.location.longitude, this.location.latitude]
-        	mapViewProperties['zoom']=12
+        	mapViewProperties['zoom']=15
         } else {
         	mapViewProperties['center']=[0, 0]
         	mapViewProperties['zoom']=2
@@ -72,44 +72,95 @@ export class EsriMapComponent implements OnInit {
 
         this.mapView = new MapView(mapViewProperties);
 
-        if(this.location) {
-            var point = new Point({
-        		longitude: this.location.longitude, 
-        		latitude: this.location.latitude}
-        	);
-        	var marker = new SimpleMarkerSymbol({
+        this.setOpenDonorFormOnClick(this.mapView);
+        
+        var donors = [
+        	{longitude:-43.187, latitude:-22.806, firstName: 'Agatha', bloodGroup: "A+"},
+        	{longitude:-43.189, latitude:-22.799, firstName: 'Bernie', bloodGroup: "B-"},
+        	{longitude:-43.173, latitude:-22.798, firstName: 'Carlos', bloodGroup: "O+"},
+        	{longitude:-43.18, latitude:-22.814, firstName: 'Dandara', bloodGroup: "AB+"}
+        ];
+        this.createMarkerForDonors(this.mapView, donors);
+
+
+      });
+    });
+  }
+
+  setOpenDonorFormOnClick(view) {
+  	var component = this;
+  	view.on("click", function(event) {
+    	var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+    	var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
+
+    	view.popup.open({
+        	title: "New Donor Information",
+        	location: event.mapPoint,
+        	content: [{
+		        	type: "fields",
+		        	fieldInfos: [{
+		          		fieldName: "First Name"
+		        	}, {
+		          		fieldName: "Last Name"
+		        	}, {
+		          		fieldName: "Contact Number"
+		        	}, {
+		          		fieldName: "Address"
+		        	}, {
+		          		fieldName: "Blood Group"
+		        	}]
+		      	}]
+    	});
+	});
+  }
+
+  createMarkerForDonors(view, donors) {
+  	var component = this;
+  	donors.forEach(function(donor, index, arr) {
+  		console.log(donor)
+  		component.createAMarkerAt(view, donor);
+  	});
+  }
+
+  createAMarkerAt(view, donor) {
+  	this.esriLoader.require([
+        'esri/Map',
+        'esri/views/MapView',
+        'esri/geometry/Point',
+        'esri/symbols/SimpleMarkerSymbol',
+        "esri/Graphic",
+  		'dojo/domReady!'
+      ], function(
+      	Map,
+        MapView, 
+        Point,
+        SimpleMarkerSymbol,
+        Graphic
+  		) {
+		    var point = new Point(donor);
+			var marker = new SimpleMarkerSymbol({
 			  	//color: "palegreen"
 			});
 
-        	var lineAtt = {
-    			Name: "Keystone Pipeline",  // The name of the pipeline
-    			Owner: "TransCanada",  // The owner of the pipeline
-    			Length: "3,456 km"  // The length of the pipeline
-  			};
+			var lineAtt = {
+				firstName: "Cindy",
+				lastName: "Moore",
+				contactNumber: "+55 21 999999999",
+				address: "Some St, 555",
+				bloodGroup: "A+"
+			};
 
-  			var polylineGraphic = new Graphic({
-    			geometry: point,   // Add the geometry created in step 4
-    			symbol: marker,   // Add the symbol created in step 5
-    			attributes: lineAtt,   // Add the attributes created in step 6
-    			popupTemplate: {
-		      	title: "{Name}",
-		      	content: [{
-		        	type: "fields",
-		        	fieldInfos: [{
-		          		fieldName: "Name"
-		        	}, {
-		          		fieldName: "Owner"
-		        	}, {
-		          		fieldName: "Length"
-		        	}]
-		      	}]
-		    	}
-  			});
-  			this.mapView.graphics.add(polylineGraphic);
-        }
-        
-      });
-    });
+			var polylineGraphic = new Graphic({
+				geometry: point,
+				symbol: marker,
+				attributes: donor,
+				popupTemplate: {
+      				title: "{firstName} ({bloodGroup})",
+					content: "<a link=\"#\">click to show</a>"
+		      	}
+			});
+			view.graphics.add(polylineGraphic);
+  	});
   }
 
 }
