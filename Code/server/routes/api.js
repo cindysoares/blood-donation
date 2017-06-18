640f7e27daad874d2fcf82626665330cd477df8a
Cindy Soares <cindy.as@gmail.com>
2017-06-18 13:47:34 -0300

    creating a service endpoint to return donors

--- Code/server/routes/api.js
+++ Code/server/routes/api.js
@@ -5,4 +5,13 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+router.get('/donors', (req, res) => {
+  return res.status(200).json([
+        	{longitude:-43.187, latitude:-22.806, firstName: 'Agatha', bloodGroup: "A+"},
+        	{longitude:-43.189, latitude:-22.799, firstName: 'Bernie', bloodGroup: "B-"},
+        	{longitude:-43.173, latitude:-22.798, firstName: 'Carlos', bloodGroup: "O+"},
+        	{longitude:-43.18, latitude:-22.814, firstName: 'Dandara', bloodGroup: "AB+"}
+        ]);
+});
+
 module.exports = router;
